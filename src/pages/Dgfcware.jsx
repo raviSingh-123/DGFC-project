12508cf5a2136662498f42b1e698826dd7775427
raviSingh-123 <ravisingh912317@gmail.com>
2024-11-19 19:33:49 +0530

    dgfcware1

--- src/pages/Dgfcware.jsx
+++ src/pages/Dgfcware.jsx
@@ -5,9 +5,9 @@ import warehouse2 from '../assets/warehouse2.jpg'
 function Dgfcware() {
   return (
     <div>
-      <div className='md:flex md:flex-2 hover:'>
+      <div className='md:flex md:flex-2'>
         <div className='px-6 py-6 group'>
-          <img className='rounded-lg' src={warehouse1} alt="warehouse1" />
+          <img className='rounded-lg ' src={warehouse1} alt="warehouse1" />
           <h1 className='lg:group-hover:block hidden text-white text-[40px] absolute top-[230px] left-[220px] font-bold group-hover:bg-black group-hover:rounded-xl p-8 font-sans duration-200'>WAREHOUSE 1</h1>
         </div>
         <div className='px-6 py-6 group'>
