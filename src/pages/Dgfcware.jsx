08ac5024364aee6fe77d45ec809089e909e5b8d6
raviSingh-123 <ravisingh912317@gmail.com>
2024-11-19 19:17:10 +0530

    dgfcwareh

--- src/pages/Dgfcware.jsx
+++ src/pages/Dgfcware.jsx
@@ -8,11 +8,11 @@ function Dgfcware() {
       <div className='md:flex md:flex-2 hover:'>
         <div className='px-6 py-6 group'>
           <img src={warehouse1} alt="warehouse1" />
-          <h1 className='md:group-hover:block hidden text-white text-[40px] absolute top-[230px] left-[220px] font-bold group-hover:bg-black group-hover:rounded-xl p-8 font-sans duration-200'>WAREHOUSE 1</h1>
+          <h1 className='lg:group-hover:block hidden text-white text-[40px] absolute top-[230px] left-[220px] font-bold group-hover:bg-black group-hover:rounded-xl p-8 font-sans duration-200'>WAREHOUSE 1</h1>
         </div>
         <div className='px-6 py-6 group'>
           <img src={warehouse2} alt="warehouse2" />
-          <h1 className='md:group-hover:block hidden text-white text-[40px] absolute top-[230px] right-[220px] font-bold group-hover:bg-black group-hover:rounded-xl p-8 font-sans duration-200'>WAREHOUSE 2</h1>
+          <h1 className='lg:group-hover:block hidden text-white text-[40px] absolute top-[230px] right-[220px] font-bold group-hover:bg-black group-hover:rounded-xl p-8 font-sans duration-200'>WAREHOUSE 2</h1>
         </div>
       </div>
     </div>
