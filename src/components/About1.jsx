9ad9dd284585240f5d60e70cc9fd810882efd1e3
tanishS123 <tanishsrivastava25@gmail.com>
2024-11-20 20:14:00 +0530

    about page

---
+++ src/components/About1.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+function About1() {
+  return (
+   <>
+   {/* vision and mission */}
+      <section>
+                <div className="pt-14 pb-14 text-center">
+                    <div className="flex justify-center">
+                        <p className="mt-3">______________ </p>
+                        <h1 className="text-[20px] md:text-[40px]  font-bold text-red-500 px-2">
+                            VISION AND MISSION
+                        </h1>
+                        <p className="mt-3">______________</p>
+                    </div>
+
+                    <p className="px-4 sm:px-12 lg:px-32 xl:px-72 py-4 sm:py-6 text-sm sm:text-base lg:text-lg">
+                    Our mission is to create satisfied customer by offering them integrated logistics services in the most effective way and to be the safest, consistent and cost efficient logistics service provider thereby ensuring long lasting business relationship and growth of both the organizations.
+
+               We want to maintain the superior quality of our fleet as per global standards and conduct our business with integrity, in compliance with applicable laws,and in manner that creates values for all our associates.
+                    </p>
+                    
+                    </div>
+                    
+                    {/* quality policy */}
+                <div className="pb-14 text-center bg-orange-200">
+                    <div className="flex justify-center">
+                        <p className="mt-3">______________ </p>
+                        <h1 className="text-[20px] md:text-[40px]  font-bold text-red-500 px-2">
+                            QUALITY POLICY
+                        </h1>
+                        <p className="mt-3">______________</p>
+                    </div>
+
+                    <p className="text-[15px] font-bold px-4 sm:px-12 lg:px-32 :px-72 py-4 sm:py-6 text-sm sm:text-base lg:text-lg">
+                    
+                    We are committed to achieve customer satisfaction by providing value added logistics services at an optimum cost along with the highest safty and quality standards and abiding by the professional ethics.
+                    </p>
+                    
+
+                    <div>
+
+                        <h1 className='text-left font-bold text-[25px] ml-9' >We shall achieve this by-</h1>
+                        <ul className='text-left'>
+                        <li className="font-semibold  text-[8px] sm:text-[20px]  sm:ml-16">
+                        ⭐ Committed to continous improvement in your systems and business practices.So that we can offer most economically services to our customer.
+                        </li>
+                        <li className="font-semibold text-[8px] sm:text-[20px]  sm:ml-16">
+                            ⭐ Being positive for attaining global standard in quality of services we offer to our key customers.
+                        </li>
+                        <li className="font-semibold  text-[8px] sm:text-[20px]  sm:ml-16">
+                            ⭐ Nurturing operational excellence in our team.
+                        </li>
+                        </ul>
+                       
+                    </div>
+                    
+                    </div>
+                    
+  </section>
+  </>
+  );
+}
+
+export default About1
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Chain from '../components/Chain'
+import About1 from '../components/About1'
 
 function About() {
   return (
     <div>
+      <About1/>
        <Chain/>
     </div>
+
   )
 }
 
